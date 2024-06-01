ca75fe4efe312b7229458bd806b7be6e5aae1258
henry <89320103+henrxG@users.noreply.github.com>
2024-06-01 23:35:25 +0200

    Fehler behoben und Fertiggestellt

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const container = document.querySelector('#container');
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    container.style.width = '100vw';
+    container.style.height = '100vh';
 
-    for (let i = 0; i < 256; i++) {
-        const element = document.createElement('div');
-        element.classList.add(`element${i}`);
-        element.style.backgroundColor = 'red';
-        element.style.width = '30px';
-        element.style.height = '30px';
-        element.style.margin = '1px';
-        container.appendChild(element);
-    }
+    document.getElementById('SquareButton').addEventListener('click', function() {
+        let SquareCount = prompt("Wie viele Rechtecke pro Seite?");
 
-    // Load CSS file after JavaScript execution
+        // Checkt ob der USerInput höher als 100 ist
+        if (SquareCount > 100) {
+            alert("DIe maximale Anzahl an Rechtecken pro Seite beträgt 100!");
+            return;
+        }
+
+        SquareCount = SquareCount * SquareCount; // berechnet die Anzahl der Rechtecke
+
+        // Löscht alle Rechtecke
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        // Generiert die Rechtecke
+        for (let i = 0; i < SquareCount; i++) {
+            const element = document.createElement('div');
+            element.classList.add(`element${i}`);
+            element.style.backgroundColor = 'gray';
+            element.style.width = `${100 / Math.sqrt(SquareCount)}%`; // Passt die Breite an die Anzahl der Rechtecke an
+            element.style.height = `${100 / Math.sqrt(SquareCount)}%`; // Passt die Höhe an die Anzahl der Rechtecke an
+            container.appendChild(element);
+
+            element.addEventListener('mouseover', function() {
+                this.style.backgroundColor = 'orange';
+            });
+        }
+    });
+
+    // Lädt Css Datei nach JavaScript
     let link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'style.css'; // Replace with the path to your CSS file
+    link.href = 'style.css'; 
     document.head.appendChild(link);
 });
